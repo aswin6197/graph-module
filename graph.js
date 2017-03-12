02fd307a7022700083b530f47665123d6b79bdcd
aswin6197 <aswin6197@gmail.com>
2017-03-12 14:00:14 +0530

    panning almost done
    just need to smoothen the transition

--- graph.js
+++ graph.js
@@ -1,15 +1,10 @@
 var NS="http://www.w3.org/2000/svg";
-
-var range;
-var coordinate;
-function copy(coord,values){
-    range = values;
-    coordinate = coord;
-}
-
+var origin = {
+    x:0,
+    y:0
+};
 //create a svg tag to add elements to it later
 function createSvg() {
-
 
     var svg = document.createElementNS(NS,"svg");
     svg.setAttribute('width',window.innerWidth);
@@ -22,11 +17,11 @@ function createSvg() {
 function lines(x1,y1,x2,y2){
 var line = document.createElementNS(NS,"line");
 
-line.setAttribute('x1',x1);
-line.setAttribute('y1',y1);
-line.setAttribute('x2',x2);
-line.setAttribute('y2',y2);
-line.setAttribute('stroke',"black");
+    line.setAttribute('x1',x1);
+    line.setAttribute('y1',y1);
+    line.setAttribute('x2',x2);
+    line.setAttribute('y2',y2);
+    line.setAttribute('stroke',"black");
 //line.setAttribute('stroke-width',10);
 return line;
 }
@@ -41,38 +36,72 @@ t.textContent = string;
 return t;
 }
 
+function border(coord){
+
+    x = lines(coord.x,coord.y,coord.x+coord.xlen,coord.y);
+    svg.appendChild(x);
+    x = lines(coord.x,coord.y,coord.x,coord.y+coord.ylen);
+    svg.appendChild(x);
+    x = lines(coord.x+coord.xlen,coord.y,coord.x+coord.xlen,coord.y+coord.ylen);
+    svg.appendChild(x);
+    x = lines(coord.x,coord.y+coord.ylen,coord.x+coord.xlen,coord.y+coord.ylen);
+    svg.appendChild(x);
+}
+
 //draws the x y axises and all the markings
 function coordinates(coord,values) {
-
-copy(coord,values);
+var t =
+origin.x =coord.x +  Math.round((0- values.xstart )/(values.xend- values.xstart)*coord.xlen);
+origin.y =coord.ylen +  coord.y - Math.round((0 - values.ystart)/(values.yend - values.ystart)*coord.ylen);
 //x axis
-var x = lines(coord.x,coord.y,coord.xend,coord.y);
+var x = lines(coord.x,origin.y,coord.x + coord.xlen,origin.y);
 svg.appendChild(x);
 
 //y axis
-x = lines(coord.x,coord.y,coord.x,coord.yend);
+x = lines(origin.x,coord.y,origin.x,coord.y + coord.ylen);
 svg.appendChild(x);
 
 //lines on x axis
-var step = (coord.xend - coord.x)/(values.xend - values.xstart);
 
-for(i = coord.x+step;i<= coord.xend;i+=step){
-    x=lines(i,coord.y-10,i,coord.y+10);
+    var temp = origin.x - 0;
+
+for(var i = 1;i<=values.xend;i++){
+    x = lines(Math.round(i)*ratio.x+temp,origin.y-10,Math.round(i)*ratio.x+temp,origin.y+10);
+    svg.appendChild(x);
+    x = text( Math.round(i)*ratio.x+temp,origin.y+20,Math.round(i));
+    svg.appendChild(x);
+}
+
+for(i = -1;i>= values.xstart;i--){
+    x = lines(Math.round(i)*ratio.x+temp,origin.y-10,Math.round(i)*ratio.x+temp,origin.y+10);
+    svg.appendChild(x);
+    x = text( Math.round(i)*ratio.x+temp,origin.y+20,Math.round(i));
     svg.appendChild(x);
 }
 
 //lines on y axis
-step = (coord.y - coord.yend)/(values.yend - values.ystart);
-for(i =coord.y-step; i>=coord.yend;i-=step){
-    x = lines(coord.x-10,i,coord.x+10,i);
+ temp = origin.y - 0;
+
+for(i = 1;i<=values.yend;i++){
+    x = lines(origin.x,temp-Math.round(i)*ratio.y,origin.x+10,temp-Math.round(i)*ratio.y);
+    svg.appendChild(x);
+    x = text(origin.x-20,temp-Math.round(i)*ratio.y,Math.round(i));
     svg.appendChild(x);
 }
+
+for(i = -1;i>=values.ystart;i--){
+    x = lines(origin.x,temp-Math.round(i)*ratio.y,origin.x+10,temp-Math.round(i)*ratio.y);
+    svg.appendChild(x);
+    x = text(origin.x-20,temp-Math.round(i)*ratio.y,Math.round(i));
+    svg.appendChild(x);
+}
+
 //adds text to bottom of graph text is 'x axis'
-x = text((coord.x+coord.xend)/2,coord.y+40,"x axis");
+x = text(coord.x+coord.xlen/2,origin.y+40,"x axis");
 svg.appendChild(x);
 
 //adds the text 'y axis' to y axis
-x = text(coord.x-90,(coord.y+coord.yend)/2,"y axis");
+x = text(origin.x-90,coord.y - coord.ylen/2,"y axis");
 //    x.setAttribute('rotate',270);
 svg.appendChild(x);
 }
@@ -89,21 +118,21 @@ svg.appendChild(x);
 //draw the curve on the graph by giving the function and end points of the graph
 function curve(coord,values,f) {
 
-
+    ratio ={
+        x : coord.xlen/(values.xend - values.xstart)/1,
+        y : coord.ylen/(values.yend - values.ystart)/1
+    };
+border(coord);
 coordinates(coord,values);
 
 var height = window.innerHeight;
 var i=values.xstart;
-ratio ={
-    x : (coord.xend - coord.x)/(values.xend - values.xstart)/1,
-    y : (coord.y  - coord.yend)/(values.yend - values.ystart)/1
-};
 
 while(i <= values.xend){
-    var x1 =  coord.x + i*ratio.x;
-    var y1 = coord.y- f(i)*ratio.y;
-    var x2 =  (i +1/ratio.x)*ratio.x +coord.x;
-    var y2 = coord.y - f((i+1/ratio.x))*ratio.y;
+    var x1 =  origin.x + i*ratio.x;
+    var y1 = origin.y - f(i)*ratio.y;
+    var x2 =  (i +1/ratio.x)*ratio.x +origin.x;
+    var y2 = origin.y - f((i+1/ratio.x))*ratio.y;
 
     x=lines(x1,y1,x2,y2);
     svg.appendChild(x);
@@ -120,16 +149,17 @@ $(document).mouseup(function(){
     var change = [];
     change[0] = epos[0]-bpos[0];
     change[1] = epos[1]-bpos[1];
+//change[0] = 0;
+    //coord.x += change[0];
+    //coord.xend += change[0];
 
-    coord.x += change[0];
-    coord.xend += change[0];
-
-    coord.y += change[1];
-    coord.yend += change[1];
+    //coord.y += change[1];
+    //coord.yend += change[1];
 
     change[0] /=ratio.x;
     change[1] /= ratio.y;
 
+console.log(change);
     values.xstart -= change[0];
     values.xend -= change[0];
 
